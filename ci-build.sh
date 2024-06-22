cc761560f59b29d4d99f97427fde9bbfe3a0f20f
Vicente <vicenteicc2008@gmail.com>
2024-06-22 14:46:36 -0400

    update EDK2 branch in CI

--- ci-build.sh
+++ ci-build.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y build-essential uuid-dev iasl git nasm crossbuild-essential-armel crossbuild-essential-armhf bc
 curdir="$PWD"
 cd ..
-git clone https://github.com/tianocore/edk2.git -b edk2-stable202405 --recursive
+git clone https://github.com/tianocore/edk2.git -b edk2-stable202305 --recursive
 git clone https://github.com/tianocore/edk2-platforms.git
 cd "$curdir"
 git submodule init
